3da691e2a99b1b46038f8a9b22639e157922887c
Emmanuel Kala <emmanuel@ushahidi.com>
2012-05-11 08:51:15 +0300

    Minor fix
    * Removes reference to "droplet_raw" which is no longer in use in the
    droplet dicts - causing a KeyError

--- semanticsqueue/semanticsqueue.py
+++ semanticsqueue/semanticsqueue.py
@@ -49,7 +49,7 @@ class SemanticsQueueWorker(Worker):
         droplet_raw = re.sub(
             r"(?is)<(script|style).*?>.*?(</\1)",
             "",
-            droplet['droplet_raw'].strip())
+            droplet['droplet_content'].strip())
 
         # Remove HTML comments. Do this before removing HTML tags because
         # comments can contain '>' characters
