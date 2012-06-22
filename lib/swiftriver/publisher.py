fe7e72379e2e5ee74668306f1fffcc224b0e876a
Brian Muita <bgm@69mb.org>
2012-06-22 14:27:01 +0300

    Publisher: reconnect to MQ

--- lib/swiftriver/publisher.py
+++ lib/swiftriver/publisher.py
@@ -8,6 +8,9 @@ from Queue import Queue
 import pika
 import logging as log
 import json
+import socket
+import time
+import random
 
 
 class Publisher(Thread):
@@ -25,40 +28,52 @@ class Publisher(Thread):
         self.durable = durable
         self.q = Queue()
         self.start()
-        log.info("%s started" % self.name)
 
     def publish(self, item, callback = None):
         self.q.put((item, callback), False)
 
     def run(self):
-        connection = pika.BlockingConnection(
-                    pika.ConnectionParameters(host=self.mq_host),
-                    pika.reconnection_strategies.SimpleReconnectionStrategy())
-        channel = connection.channel()
-
-        if self.queue_name is not None:
-            channel.queue_declare(queue=self.queue_name, durable=self.durable)
-
-        if self.exchange_name == '':
-            self.routing_key = self.queue_name
-        else:
-            channel.exchange_declare(exchange=self.exchange_name,
-                                     type=self.exchange_type,
-                                     durable=self.durable)
-
-        props = None
-        if self.durable:
-            props = pika.BasicProperties(delivery_mode=2)
-
+        # Connect to the MQ, retry on failure
         while True:
-            item, callback = self.q.get(True)
-            channel.basic_publish(exchange=self.exchange_name,
-                                  routing_key=self.routing_key,
-                                  properties=props,
-                                  body=json.dumps(item))
-            
-            if callback is not None:
-                callback(item)
+            log.info("%s started" % self.name)
+            try:
+                connection = pika.BlockingConnection(
+                            pika.ConnectionParameters(host=self.mq_host),
+                            pika.reconnection_strategies.SimpleReconnectionStrategy())
+                channel = connection.channel()
+                
+                if self.queue_name is not None:
+                    channel.queue_declare(queue=self.queue_name, durable=self.durable)
+                
+                if self.exchange_name == '':
+                    self.routing_key = self.queue_name
+                else:
+                    channel.exchange_declare(exchange=self.exchange_name,
+                                             type=self.exchange_type,
+                                             durable=self.durable)
+                
+                props = None
+                if self.durable:
+                    props = pika.BasicProperties(delivery_mode=2)
+                
+                while True:
+                    item, callback = self.q.get(True)
+                    channel.basic_publish(exchange=self.exchange_name,
+                                          routing_key=self.routing_key,
+                                          properties=props,
+                                          body=json.dumps(item))
+                    
+                    if callback is not None:
+                        callback(item)
+            except socket.error, msg:
+                log.error("%s error connecting to the MQ: %s. Retrying..." %
+                          (self.name, msg))
+                time.sleep(60 + random.randint(0, 120))
+            except pika.exceptions.AMQPConnectionError, e:
+                log.error("%s lost connection to the MQ, reconnecting" %
+                          self.name)
+                log.exception(e)
+                time.sleep(60 + random.randint(0, 120))
 
 
 class DropPublisher(Publisher):
